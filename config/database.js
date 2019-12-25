b02047bf852d52d94a3343a150676df8230872fa
Akhil Kumar E <eeturiakhil@gmail.com>
2019-12-26 00:33:01 +0530

    Linked MLAB DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: "mongodb://localhost:27017/meanauth",
+  // database: "mongodb://localhost:27017/meanauth",
+  database: "mongodb://akhile:AK1234@ds263107.mlab.com:63107/meanauthapp",
   secret: "secret"
 };
