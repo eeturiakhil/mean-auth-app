60f89c93be7b9f237c93a2777d0b775cf2592db7
Akhil Kumar E <eeturiakhil@gmail.com>
2019-12-25 18:36:52 +0530

    Backend Code

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');;
-const cors = require('cors');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const config = require('./config/database');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
 
 // Connect to Database
-mongoose.connect(config.database);
+mongoose.connect(config.database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // On Connection
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to Database ${config.database}`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to Database ${config.database}`);
 });
 
 // On Error
-mongoose.connection.on('error', (err) => {
-    console.log(`Database Error ${err}`);
+mongoose.connection.on("error", err => {
+  console.log(`Database Error ${err}`);
 });
 
 const app = express();
 
-const users = require('./routes/users');
+const users = require("./routes/users");
 
 // Port Number
 const port = 3000;
@@ -30,20 +33,26 @@ const port = 3000;
 app.use(cors());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./config/passport")(passport);
+
 // Users route
-app.use('/users', users);
+app.use("/users", users);
 
 // Index route
-app.get('/', (req, res) => {
-    res.send('Invalid request');
+app.get("/", (req, res) => {
+  res.send("Invalid request");
 });
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://54.153.202.254:27017/co_op_data',
-    secret: 'S`(P}4^RJU8sr.?'
-}
+  database: "mongodb://localhost:27017/meanauth",
+  secret: "secret"
+};
