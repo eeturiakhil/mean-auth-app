2b7728f02f05fadd9c545e234ab5fbaf660ade46
Akhil Kumar E <eeturiakhil@gmail.com>
2020-01-03 01:11:29 +0530

    Updated DB URL

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   // database: "mongodb://localhost:27017/meanauth",
-  // database: "mongodb://akhile:AK1234@ds263107.mlab.com:63107/meanauthapp",
-  database:
-    "mongodb://akhile:AK1431@cluster0-ilqni.mongodb.net/test?retryWrites=true&w=majority",
+  database: "mongodb://akhile:AK1234@ds263107.mlab.com:63107/meanauthapp",
+  // database:
+  // "mongodb://akhile:AK1431@cluster0-ilqni.mongodb.net/test?retryWrites=true&w=majority",
   secret: "secret"
 };
